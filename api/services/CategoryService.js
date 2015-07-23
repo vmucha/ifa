9307b2fe25ca9934b05612019ccc5cb9f6a12dee
Mucha <vmucha@XIN852646.asv.local>
2015-07-23 14:18:47 +0200

    escenic feeds

--- api/services/CategoryService.js
+++ api/services/CategoryService.js
@@ -2,25 +2,28 @@ module.exports = {
 	update: function (req) {
 			console.log("hier bin ich2",req.param('category'));
 			var cat = ""; // default feed
+			var catId = req.param('category');
 			switch(req.param('category')) {
-				case "0": 
+				case "1": 
 					cat = "politik";
 					break;
-				case "1":
+				case "2":
 					cat = "sport";
 					break;
-				case "2":
+				case "3":
 					cat = "wirtschaft";
 					break;
-				case "3":
+				case "4":
 					cat = "geschichte";
 					break;
 				default:
 					cat = "politik";
+					catId = 1;
 			}
 			console.log(cat);
 			var request = require('request');
-			var url = "http://www.welt.de/reportage/?config=ifa_glossar_1";
+			var url = "http://www.welt.de/reportage/?config=ifa_glossar_"+catId;
+			console.log(url);
 			request(url, function (error, response, body) {
 					  if (!error && response.statusCode == 200) {
 					  		var bdata = JSON.parse(body);
