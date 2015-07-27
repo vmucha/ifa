97af6d64f18ff5136e703a848dd54006b0423516
Mucha <vmucha@XIN852646.asv.local>
2015-07-27 16:01:24 +0200

    template for cards

--- assets/app/directives/articlecards.js
+++ assets/app/directives/articlecards.js
@@ -3,7 +3,35 @@ app.directive("articleCards",function () {
 		restrict:"EA",
     scope:false,
 		//replace:true,
-		template:'<li on-last-repeat class="articleCard col-sm-6 col-md-4 col-lg-3" ng-repeat="article in articles"><div class="card"><a class="original" href="{{article.url}}">Original</a><a ui-sref="glossar.article({articleId:{{article.id}},category:\'{{category}}\'})"><h4>{{article.kicker}}</h4><h3>{{article.title}}</h3><img ng-src="{{article.teaserImage}}"/></a></div></li>'
+    template: '<div ng-repeat="article in articles" class="col-md-4 col-sm-6 col-xs-6 card-item-container">'+
+          '<article class="card-item js-card-item news">'+
+          '<figure>'+
+            '<a ui-sref="glossar.article({articleId:{{article.id}},category:\'{{category}}\'})" name="_PE_angst_glossarcard_more_image_">'+
+                    '<img ng-src="{{article.teaserImage}}" alt="{{article.title}}" class="img-responsive">'+
+                '</a>'+
+            '</figure>'+
+        
+
+        '<span name="_PE_angst_glossarcard_tag_" title="{{article.kicker}}" class="card-item-tag">{{article.kicker}}</span>'+
+        
+
+        
+
+        
+         '   <a class="js-open-overlay" ui-sref="glossar.article({articleId:{{article.id}},category:\'{{category}}\'})" name="_PE_angst_glossarcard_more_link_">'+
+                
+            '<h2 class="card-item-title">{{article.title}}</h2>'+
+            '</a>'+
+        
+
+        '<a class="card-item-link js-open-overlay" ui-sref="glossar.article({articleId:{{article.id}},category:\'{{category}}\'})" name="_PE_angst_glossarcard_more_link_">Ansehen'+
+            
+        '</a>'+
+
+        
+    '</article></div>'
+
+	//	template:'<li on-last-repeat class="articleCard col-sm-6 col-md-4 col-lg-3" ng-repeat="article in articles"><div class="card"><a class="original" href="{{article.url}}">Original</a><a ui-sref="glossar.article({articleId:{{article.id}},category:\'{{category}}\'})"><h4>{{article.kicker}}</h4><h3>{{article.title}}</h3><img ng-src="{{article.teaserImage}}"/></a></div></li>'
 		
         	//$timeout(scope.resize, 0);  //Calling a scoped method
         	
@@ -18,3 +46,9 @@ app.directive("articleCards",function () {
 
 });
 
+
+
+
+
+        
+           
