9f1a4f5d7e36b0801f5824b2a3a4e18282375a2d
Frederik Merz <frederik.merz@weltn24.de>
2015-07-27 17:09:25 +0200

    Moved Article-Feed-Building from FE to BE

--- api/controllers/ArtikelController.js
+++ api/controllers/ArtikelController.js
@@ -6,136 +6,38 @@
  */
 
 module.exports = {
-	getArticles: function(req, res) {
-		//var art = req.params;
-						console.log("hier bin ich123");
+    getArticles: function (req, res) {
 
-		Artikel.find().exec(function(err, data){
-			console.log(data)
-		});
+        Artikel.find().exec(function (err, data) {
+            console.log(data)
+        });
 
+        res.send("test2");
+    },
 
-		//console.log(Artikel.url);
-		res.send("test2");
-	},
+    show: function (req, res) {
+        console.log("hier bin ich1");
 
-	show:function(req,res) {
-						console.log("hier bin ich1");
+        Artikel.find().exec(function (err, data) {
+            res.view("overview", {articles: data});
+        });
+    },
 
-		Artikel.find().exec(function(err,data) {
-			res.view("overview",{articles:data});
-		});	
-	},
-
-	getContent: function(req, res) {		
-			var result;
-			//Artikel.find({where: {id:"143821894"}}).exec(function(err,data) {
-				/*console.log(req.param("id"));
-				Artikel.find({where: {id:req.param('id')}}).exec(function(err,data) {
-					console.log(data);
-				result = data;
-				res.send("ok");
-				//res.view('result',{metatitle:data[0].title,currentArticle:data[0]});
-			});
-		//res.send(req.param('id'));*/
-
-			var request = require('request');
-			request('http://www.welt.de/article'+req.param('id')+'/?noredirect=true&config=jsn&'+Math.random(), function (error, response, body) {
-					  if (!error && response.statusCode == 200) {
-					  		var bdata = JSON.parse(body);
-					  		res.send(bdata.articles);
-					  	}
-				});
-		}
-
-
-	};
-					  		/*for(var i=0;i<bdata.articles.length;i++) {
-					  			var art = bdata.articles[i];
-					  			art.teaserImage = art.teaserImage.replace("w1-h1","w400-h400-oo");
-					  			Artikel.create(art).exec(function(err, data){
-					  				length--;
-					  				console.log("success, gespeichert",length);
-					  				if(!length) {
-					  					  Artikel.find().exec(function(err,data) {
-												//res.view("overview",{articles:data});
-										});	
-					  				}
-					  			});
-
-	}
-
-
-
-
-
-
+    getContent: function (req, res) {
+        var request = require('request');
+        request('http://www.welt.de/article' + req.param('id') + '/?noredirect=true&config=jsn&' + Math.random(), function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var bdata = JSON.parse(body);
+                bdata.articles[0].processedParagraphs = [];
+                bdata.articles[0].body = '';
+                for(var i=0;i<bdata.articles[0].paragraphs.length;i++) {
+                    for(var u=0;u<bdata.articles[0].paragraphs[i].parts.length;u++) {
+                        bdata.articles[0].processedParagraphs.push(bdata.articles[0].paragraphs[i].parts[u]);
+                    }
+                    bdata.articles[0].body += bdata.articles[0].paragraphs[i].openinigTag+bdata.articles[0].paragraphs[i].content+bdata.articles[0].paragraphs[i].openinigTag.replace("<","</");
+                }
+                res.send(bdata.articles);
+            }
+        });
+    }
 };
-
-
-
-
-
-
-
-/*
-module.exports = {
-	update: function (req) {
-			console.log("hier bin ich2",req.param('category'));
-			var cat = ""; // default feed
-			switch(req.param('category')) {
-				case "0": 
-					cat = "politik";
-					break;
-				case "1":
-					cat = "sport";
-					break;
-				case "2":
-					cat = "wirtschaft";
-					break;
-				case "3":
-					cat = "geschichte";
-					break;
-				default:
-					cat = "politik";
-			}
-			console.log(cat);
-			var request = require('request');
-			request('http://www.welt.de/'+cat+'/?config=iphone_ressort', function (error, response, body) {
-					  if (!error && response.statusCode == 200) {
-					  		var bdata = JSON.parse(body);
-					  		var length = bdata.articles.length;
-					  		var ids=[];
-					  		for(var i=0;i<length;i++) {
-					  			bdata.articles[i].teaserImage = bdata.articles[i].teaserImage.replace("w1-h1","w400-h400-oo");
-					  			ids.push(bdata.articles[i].id);
-					  		}
-					  		Artikel.create(bdata.articles).exec(function(err,data) {
-					  			console.log("articles angelegt");
-					  			Category.create({
-					  				name:cat,
-					  				url:"http://www.welt.de/"+cat+"/?config=iphone_ressort",
-					  				id:req.param('category'),
-					  				articles:ids
-					  			}).exec(function(err,data) {
-					  				console.log("category angelegt");
-					  			})
-					  		});
-					  		/*for(var i=0;i<bdata.articles.length;i++) {
-					  			var art = bdata.articles[i];
-					  			art.teaserImage = art.teaserImage.replace("w1-h1","w400-h400-oo");
-					  			Artikel.create(art).exec(function(err, data){
-					  				length--;
-					  				console.log("success, gespeichert",length);
-					  				if(!length) {
-					  					  Artikel.find().exec(function(err,data) {
-												//res.view("overview",{articles:data});
-										});	
-					  				}
-					  			});
-					  		}*/
-
-					  		//var urlparts = bdata.data.result.items[0].url.split('/');
-					  		//var name = urlparts[urlparts.length-1];
-					  	//	console.log("erfolg!!",bdata);
-					  		//self.saveNetwork(data[idindex].id, 'twitter', name, null);
--- assets/app/controllers/ArticleController.js
+++ assets/app/controllers/ArticleController.js
@@ -5,63 +5,26 @@ app.controller('ArticleController', ['$scope','articleService','$stateParams','$
     $scope.headline = "";
     $scope.intro = "";
     $scope.articleType = "";
-    $scope.prepareOverlay = function(id) {
-        articleService.getContent(articleId,function(data){
-            $scope.content = data[0];
-            for(var i=0;i<data[0].paragraphs.length;i++) {
-                for(var u=0;u<data[0].paragraphs[i].parts.length;u++) {
-                    $scope.paragraphs.push(data[0].paragraphs[i].parts[u]);    
-                }
-               $scope.body += data[0].paragraphs[i].content;
-               console.log("body",$scope.body);
-            }
-        });
-    };
-    var articleId = $stateParams.articleId
+
+    var articleId = $stateParams.articleId;
     console.log(articleId);
-//var articleId = 144213754;
+
     function init() { 
         console.log("init art");
         articleService.getContent(articleId,function(data){
             console.log("cb2 art",data);
-            //$scope.content = getRaw(data[0].paragraphs[0].parts[1].content);
-            
             var data = data[0];
             $scope.content = data;
             $scope.articleType = data.type;
             $scope.headline = data.title;
             $scope.intro = data.intro;
             $scope.kicker = data.kicker;
-            for(var i=0;i<data.paragraphs.length;i++) {
-                for(var u=0;u<data.paragraphs[i].parts.length;u++) {
-                    $scope.paragraphs.push(data.paragraphs[i].parts[u]);    
-                }
-                $scope.body += data.paragraphs[i].openinigTag+data.paragraphs[i].content+data.paragraphs[i].openinigTag.replace("<","</");
-            }
-            console.log("body",$scope.body);
+            $scope.paragraphs = data.processedParagraphs;
+            $scope.body = data.body;
         });
     }
     init();
-
-     
-
-
      $scope.to_trusted = function(html_code) {
         return $sce.trustAsHtml(html_code);
     };
-    /*$scope.articles = {};
-    $scope.loadArticles = {loading: true};
-
-    function init() { 
-
-        overviewService.listArticles(function(data){
-            $scope.loadArticles.loading = false;
-            for(var i=0;i<data.length;i++) {
-                //data[i].teaserImage = data[i].teaserImage.replace("w1-h1","w400-h400-oo");
-            }
-            $scope.articles = data;
-        });
-        console.log("init");
-    }
-    //init();*/
 }]);
